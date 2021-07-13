468f39950471d6c45a7c6586cf12e256515b2a3a
touka.hanetsuki <touka.hanetuki@gmail.com>
2021-07-13 11:51:42 +0900

    add: update enviroment vite

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+
+export default defineConfig({
+  root: path.relative(__dirname, 'src'),
+  publicDir: path.relative(__dirname, 'src/static'),
+  build: {
+    outDir: path.relative(__dirname, 'public')
+  },
+})
